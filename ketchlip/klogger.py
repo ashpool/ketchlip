92a68c90a941add723c7e35944a944686835cd19
Magnus Ljadas <magnus.ljadas@deltaprojects.se>
2012-04-08 19:39:37 +0200

    Added logging module

--- ketchlip/klogger.py
+++ ketchlip/klogger.py
@@ -1,1 +1,22 @@
-__author__ = 'magnus'
+import logging
+
+logger = logging.getLogger('ketchlip')
+logger.setLevel(logging.DEBUG)
+
+fh = logging.FileHandler('ketchlip.log')
+fh.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+
+def info(msg, *args, **kvargs):
+    logger.info(msg, *args, **kvargs)
