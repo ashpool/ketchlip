293e9641eaa9abbd227571b90a8307c129bb8d32
Magnus Ljadas <magnus.ljadas@deltaprojects.se>
2012-04-10 09:41:40 +0200

    Adding optional base directory parameter on load method

--- ketchlip/dynamic_content_loader.py
+++ ketchlip/dynamic_content_loader.py
@@ -3,8 +3,8 @@
 
 class DynamicContentLoader:
 
-    def load(self, page):
-        f = open("./www/" + page, "r")
+    def load(self, page, base_dir = "./www/"):
+        f = open(base_dir + page, "r")
         text = f.read()
         f.close()
         return text
