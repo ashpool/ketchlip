bf81c444cefe3fb098b22f07b19343430a1887a0
Magnus Ljadas <magnus.ljadas@deltaprojects.se>
2012-04-11 19:48:18 +0200

    Persisting last status

--- ketchlip/tweet_scanner.py
+++ ketchlip/tweet_scanner.py
@@ -5,27 +5,7 @@ import time
 import tweepy
 import codecs
 from ketchlip import klogger
-
-def process_text(text, tweets):
-    BATCH_SIZE = 5 # todo is BATCH_SIZE really needed?
-    try:
-        links = get_all_links(text)
-        if len(links) >= 0:
-            for link in links:
-                tweets.append(link)
-
-            if len(tweets) > BATCH_SIZE:
-                klogger.info("Writing tweets to file")
-                f = codecs.open('/tmp/tweets.txt', 'a', 'utf-8')
-                for tweet in tweets:
-                    f.write(tweet + '\n')
-                f.close()
-                tweets = []
-        return tweets
-
-    except Exception as e:
-        klogger.error('Encountered Exception: ' + str(e))
-        return tweets
+from ketchlip.persister import Persister
 
 
 def get_all_links(text):
@@ -39,27 +19,35 @@ def get_all_links(text):
             links.append(link[0])
     return links
 
-
-lastStatusProcessed = None
-
-def scan(api):
-    lastStatusProcessed = None
-
+def scan(api, last_status_processed = None):
+    BATCH_SIZE = 5
     tweets = []
 
     while True:
-        for status in tweepy.Cursor(api.friends_timeline, since_id=lastStatusProcessed).items(20):
-            if lastStatusProcessed is None:
-                lastStatusProcessed = status.id
+        for status in tweepy.Cursor(api.friends_timeline, since_id=last_status_processed).items(20):
+            if last_status_processed is None:
+                last_status_processed = status.id
                 break
 
-            if status.id > lastStatusProcessed:
-                lastStatusProcessed = status.id
+            if status.id > last_status_processed:
+                last_status_processed = status.id
 
-            tweets = process_text(status.text, tweets)
+            links = get_all_links(status.text)
+
+            if len(links) >= 0:
+                for link in links:
+                    tweets.append(link)
+
+                    if len(tweets) > BATCH_SIZE:
+                        klogger.info("Writing tweets to file")
+                        f = codecs.open('/tmp/tweets.txt', 'a', 'utf-8')
+                        for tweet in tweets:
+                            f.write(tweet + '\n')
+                        f.close()
+                        Persister("/tmp/last_status_processed.txt").save(last_status_processed)
+                        tweets = []
 
         time.sleep(60)
-
 
 def main():
     try:
@@ -77,14 +65,11 @@ def main():
         api = tweepy.API(auth)
 
         klogger.info(str("Scanning " + api.me().name + " friends timeline"))
-
-        scan(api)
+        last_status_processed = Persister("/tmp/last_status_processed.txt").load()
+        scan(api, last_status_processed = last_status_processed)
     except KeyboardInterrupt:
         klogger.info('^C received, shutting down tweet scanner')
-        print "lastStatusProcessed", lastStatusProcessed
 
-    except Exception, e:
-        klogger(e)
 
 if __name__ == '__main__':
     main()
