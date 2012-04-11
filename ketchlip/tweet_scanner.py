99a63f61d65d2b5de5a599109dd4684c64032607
Magnus Ljadas <magnus.ljadas@deltaprojects.se>
2012-04-11 18:12:12 +0200

    Removing streaming api, pulling from friends timeline instead

--- ketchlip/tweet_scanner.py
+++ ketchlip/tweet_scanner.py
@@ -1,96 +1,90 @@
 #-*- coding: utf-8 -*-
-
 import ConfigParser
 import re
-import sys
+import time
 import tweepy
 import codecs
 from ketchlip import klogger
 
+def process_text(text, tweets):
+    BATCH_SIZE = 5 # todo is BATCH_SIZE really needed?
+    try:
+        links = get_all_links(text)
+        if len(links) >= 0:
+            for link in links:
+                tweets.append(link)
 
-class CustomStreamListener(tweepy.StreamListener):
+            if len(tweets) > BATCH_SIZE:
+                klogger.info("Writing tweets to file")
+                f = codecs.open('/tmp/tweets.txt', 'a', 'utf-8')
+                for tweet in tweets:
+                    f.write(tweet + '\n')
+                f.close()
+                tweets = []
+        return tweets
 
-    def __init__(self, api=None):
-        super(CustomStreamListener, self).__init__(api)
-        self.tweets = []
+    except Exception as e:
+        klogger.error('Encountered Exception: ' + str(e))
+        return tweets
 
-    def get_all_links(self, text):
-        links = []
-        pattern = r'((https?):((//)|(\\\\))+[\w\d:#@%/;$()~_?\+-=\\\.&]*)'
-        expression = re.compile(pattern)
-        result = expression.findall(text)
-        if result:
-            for link in result:
-                klogger.info("FETCH: " + text)
-                links.append(link[0])
-        return links
 
-    def on_status(self, status):
-        BATCH_SIZE = 10
-        try:
-            if status.text.find("RT") >= 0:
-                # we're not interested in redundant RT spam
-                return
+def get_all_links(text):
+    links = []
+    pattern = r'((https?):((//)|(\\\\))+[\w\d:#@%/;$()~_?\+-=\\\.&]*)'
+    expression = re.compile(pattern)
+    result = expression.findall(text)
+    if result:
+        for link in result:
+            klogger.info("FETCH: " + text)
+            links.append(link[0])
+    return links
 
-            links = self.get_all_links(status.text)
-            if len(links) >= 0:
-                for link in links:
-                    self.tweets.append(link)
 
-                if len(self.tweets) > BATCH_SIZE:
-                    klogger.info("Writing tweets to file")
-                    f = codecs.open('/tmp/tweets.txt', 'a', 'utf-8')
-                    for tweet in self.tweets:
-                        f.write(tweet + '\n')
-                    f.close()
-                    self.tweets = []
+lastStatusProcessed = None
 
-        except Exception as e:
-            klogger.error('Encountered Exception: ' + str(e))
-            pass
+def scan(api):
+    lastStatusProcessed = None
 
-    def on_error(self, status_code):
-        klogger.error('Encountered error with status code:' +  str(status_code))
-        return True # Don't kill the stream
+    tweets = []
 
-    def on_timeout(self):
-        klogger.error('Timeout...')
-        return True # Don't kill the stream
+    while True:
+        for status in tweepy.Cursor(api.friends_timeline, since_id=lastStatusProcessed).items(20):
+            if lastStatusProcessed is None:
+                lastStatusProcessed = status.id
+                break
+
+            if status.id > lastStatusProcessed:
+                lastStatusProcessed = status.id
+
+            tweets = process_text(status.text, tweets)
+
+        time.sleep(60)
 
 
 def main():
-    global streaming_api
-
     try:
         cfg = ConfigParser.ConfigParser()
 
         cfg.read("../ketchlip.cfg")
-        CONSUMER_KEY=cfg.get('Twitter', 'CONSUMER_KEY')
-        CONSUMER_SECRET=cfg.get('Twitter', 'CONSUMER_SECRET')
-        ACCESS_TOKEN=cfg.get('Twitter', 'ACCESS_TOKEN')
-        ACCESS_TOKEN_SECRET=cfg.get('Twitter', 'ACCESS_TOKEN_SECRET')
+        CONSUMER_KEY = cfg.get('Twitter', 'CONSUMER_KEY')
+        CONSUMER_SECRET = cfg.get('Twitter', 'CONSUMER_SECRET')
+        ACCESS_TOKEN = cfg.get('Twitter', 'ACCESS_TOKEN')
+        ACCESS_TOKEN_SECRET = cfg.get('Twitter', 'ACCESS_TOKEN_SECRET')
 
         auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
         auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
         api = tweepy.API(auth)
 
-        friends_ids = []
-        for friend in tweepy.Cursor(api.friends).items():
-            friends_ids.append(friend.id)
+        klogger.info(str("Scanning " + api.me().name + " friends timeline"))
 
-        klogger.info(str("Scanning " + api.me().name + " " + str(len(friends_ids))) + " friends")
-
-        csl = CustomStreamListener()
-        streaming_api = tweepy.streaming.Stream(auth, csl, timeout=60)
-        streaming_api.filter(follow=friends_ids)
-
+        scan(api)
     except KeyboardInterrupt:
         klogger.info('^C received, shutting down tweet scanner')
-        if streaming_api:
-            streaming_api.disconnect()
-    except:
-        klogger.error("Unexpected error: " + str(sys.exc_info()[0]))
+        print "lastStatusProcessed", lastStatusProcessed
+
+    except Exception, e:
+        klogger(e)
 
 if __name__ == '__main__':
     main()
