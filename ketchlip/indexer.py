54fdb372a194d8a132f8d5f1df0fe4e9bcd0b8c4
Magnus Ljadas <magnus.ljadas@deltaprojects.se>
2012-04-20 10:12:34 +0200

    Refactored

--- ketchlip/indexer.py
+++ ketchlip/indexer.py
@@ -1,100 +1,46 @@
 #-*- coding: utf-8 -*-
 import ConfigParser
 from HTMLParser import HTMLParseError
-import urllib2
 import re
 import gevent
 from gevent.queue import Queue
-import time
+from crawler import Crawler
 from ketchlip_html_parser import KetchlipHTMLParser
 import klogger
-
 from persister import Persister
 from word import Word
 
-class Crawler:
-    URL = "URL"
-    EXPANDED_URL = "EXPANDED_URL"
-    TITLE = "TITLE"
-    TEXT = "TEXT"
-    LINKS = "LINKS"
-    CONTENT = "CONTENT"
-    STATUS = "STATUS" # OK or FAILED
-
-    # todo something seems to block the crawler - does http://t.co/ allow more than one browser or is the fault here?
-    def gevent_crawl(self, name):
-        while not input_queue.empty():
-            start = time.time()
-            url = input_queue.get()
-
-            gevent.sleep(0)
-            result = self.crawl(url)
-            if result:
-                output_queue.put_nowait(result)
-            elapsed = (time.time() - start)
-            klogger.info("Crawler " + name + " crawled " + url + " in " + str(elapsed) + " seconds")
-
-    def crawl(self, url):
-        result = {}
-        result[Crawler.URL] = url.strip()
-        result[Crawler.STATUS] = "FAILED"  # let's start out with some pessimism ;-)
-
-        content, expanded_url = self.get_page(url)
-        if content and expanded_url:
-            result[Crawler.EXPANDED_URL] = expanded_url.strip()
-            result[Crawler.CONTENT] = content.strip()
-            result[Crawler.STATUS] = "OK"
-
-        return result
-
-
-    def get_page(self,  url):
-        """
-        Opens the url and return its content and expanded url.
-        """
-        try:
-            response = urllib2.urlopen(url)
-            html = response.read()
-            expanded_url = response.url
-
-            return html, expanded_url
-        except urllib2.HTTPError, e:
-            klogger.info(e)
-        except Exception, e:
-            klogger.exception(e)
-        return None, None
 
 class Indexer:
 
     def __init__(self):
+        # todo introduce ignore words
         #self.ignorewords=set(['the','of','to','and','a','in','is','it'])
         self.URL_INDEX_POS = 0
         self.EXPANDED_URL_POS = 1
         self.TITLE_POS = 2
-        self.TEXT_POS = 3
-
-
+        self.DESCRIPTION_POS = 3
         self.lookup_url = {} # {url: [expanded url, title, content (100 chars)]}
         self.graph = {}  # <url>, [list of pages it links to]
         self.index = {}
-
-    def gevent_index(self, input_queue, result_queue):
         self.done = False
 
+    def gevent_index(self, input_queue, result_queue):
         gevent.sleep(30) # give the crawlers some heads up
 
-        while not (input_queue.empty() and result_queue.empty()):
+        while not input_queue.empty() or not result_queue.empty():
             result = result_queue.get(timeout=15) # the crawlers should be able to produce output below this threshold
             if result[Crawler.STATUS] == "OK":
                 self.indexing(result)
             gevent.sleep(0)
 
-        self.url_lookup = dict((v[self.URL_INDEX_POS], [k, v[self.EXPANDED_URL_POS], v[self.TITLE_POS], v[self.TEXT_POS]]) for k, v in self.lookup_url.iteritems())
+        self.url_lookup = dict((v[self.URL_INDEX_POS], [k, v[self.EXPANDED_URL_POS], v[self.TITLE_POS], v[self.DESCRIPTION_POS]]) for k, v in self.lookup_url.iteritems())
         assert len(self.url_lookup) == len(self.lookup_url)
         self.done = True
 
     def indexing(self, result):
         try:
+            DESCRIPTION_MAX_LENGTH = 160
             url = result[Crawler.URL].strip()
 
             if url in self.lookup_url:
@@ -105,15 +51,13 @@ class Indexer:
             parser = KetchlipHTMLParser(result[Crawler.CONTENT])
             title = parser.title()
             text = parser.text()
-            short_text = text
-            if len(short_text) > 100:
-                short_text = short_text[:100]
+            description = parser.description(DESCRIPTION_MAX_LENGTH)
 
             self.lookup_url[url] = [len(self.lookup_url.items()), "", "", ""]
             self.lookup_url[url][self.EXPANDED_URL_POS] = result[Crawler.EXPANDED_URL]
             self.lookup_url[url][self.TITLE_POS] = title
-            self.lookup_url[url][self.TEXT_POS] = short_text
-            self.add_page_to_index(self.index, self.lookup_url[url][self.URL_INDEX_POS], text)
+            self.lookup_url[url][self.DESCRIPTION_POS] = description
+            self.add_page_to_index(self.index, self.lookup_url[url][self.URL_INDEX_POS], " ".join([title, description, text]))
 
             if Crawler.LINKS in result:
                 self.graph[url] = result[Crawler.LINKS]
@@ -148,8 +92,6 @@ class Indexer:
         else:
             index[keyword] = [[pos, url]]
 
-input_queue = Queue()
-output_queue = Queue()
 
 # todo refactor this
 def main():
@@ -161,6 +103,9 @@ def main():
     * url_lookupfile: dictionary containing url ids - {url_id: url}
     """
     try:
+        input_queue = Queue()
+        output_queue = Queue()
+
         cfg = ConfigParser.ConfigParser()
         cfg.read("./ketchlip.cfg")
 
@@ -194,23 +139,31 @@ def main():
             klogger.info("Since " + str(since))
 
         url_list = open(tweetfile, "r")
+        include_count = 0
+        exclude_count = 0
         for timestamp_url in url_list:
             timestamp, url = timestamp_url.split("\t")
             url = url.strip()
             if not url in lookup_url and (not since or since <= timestamp):
                 input_queue.put_nowait(url)
-                klogger.info("Including " + url)
                 since = timestamp
+                include_count += 1
             else:
-                klogger.info("Excluding " + url)
+                exclude_count += 1
+
+        klogger.info("Including: " + str(include_count) + " Excluding: " + str(exclude_count))
+
 
         # Spawn off multiple crawlers and one indexer
         gevent.joinall([
-            gevent.spawn(Crawler().gevent_crawl, "A"),
-            gevent.spawn(Crawler().gevent_crawl, "B"),
-            gevent.spawn(Crawler().gevent_crawl, "C"),
-            gevent.spawn(Crawler().gevent_crawl, "D"),
-            gevent.spawn(Crawler().gevent_crawl, "E"),
+            gevent.spawn(Crawler().gevent_crawl, input_queue, output_queue,  "A"),
+            gevent.spawn(Crawler().gevent_crawl, input_queue, output_queue, "B"),
+            gevent.spawn(Crawler().gevent_crawl, input_queue, output_queue, "C"),
+            gevent.spawn(Crawler().gevent_crawl, input_queue, output_queue, "D"),
+            gevent.spawn(Crawler().gevent_crawl, input_queue, output_queue, "E"),
+            gevent.spawn(Crawler().gevent_crawl, input_queue, output_queue, "F"),
+            gevent.spawn(Crawler().gevent_crawl, input_queue, output_queue, "G"),
+            gevent.spawn(Crawler().gevent_crawl, input_queue, output_queue, "H"),
             gevent.spawn(indexer.gevent_index, input_queue, output_queue)
             ])
 
