473a536d9fc350c44b75eeb0533afb008ef37fc9
Magnus Ljadas <magnus.ljadas@deltaprojects.se>
2012-04-20 10:09:19 +0200

    Extracting Crawler from indexer.py

---
+++ ketchlip/crawler.py
@@ -0,0 +1,56 @@
+#-*- coding: utf-8 -*-
+import urllib2
+import gevent
+import time
+import klogger
+
+
+class Crawler:
+    URL = "URL"
+    EXPANDED_URL = "EXPANDED_URL"
+    TITLE = "TITLE"
+    TEXT = "TEXT"
+    LINKS = "LINKS"
+    CONTENT = "CONTENT"
+    STATUS = "STATUS" # OK or FAILED
+
+    def gevent_crawl(self, input_queue, output_queue, name):
+        while not input_queue.empty():
+            start = time.time()
+            url = input_queue.get_nowait()
+
+            gevent.sleep(0)
+            result = self.crawl(url)
+            if result:
+                output_queue.put_nowait(result)
+            elapsed = (time.time() - start)
+            klogger.info("Crawler " + name + " crawled " + url + " in " + str(elapsed) + " seconds")
+
+    def crawl(self, url):
+        result = {}
+        result[Crawler.URL] = url.strip()
+        result[Crawler.STATUS] = "FAILED"  # let's start out with some pessimism ;-)
+
+        content, expanded_url = self.get_page(url)
+        if content and expanded_url:
+            result[Crawler.EXPANDED_URL] = expanded_url.strip()
+            result[Crawler.CONTENT] = content.strip()
+            result[Crawler.STATUS] = "OK"
+
+        return result
+
+    def get_page(self,  url):
+        """
+        Opens the url and return its content and expanded url.
+        """
+        try:
+            response = urllib2.urlopen(url) # this is blocking, probably http://t.co/ does not allow more than one browser
+            html = response.read()
+            expanded_url = response.url
+
+            return html, expanded_url
+        except urllib2.HTTPError, e:
+            klogger.info(e)
+        except Exception, e:
+            klogger.exception(e)
+        return None, None
