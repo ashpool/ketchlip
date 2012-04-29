041224e4715558065f5eb47206a4a8669918c58f
Magnus Ljadas <magnus.ljadas@deltaprojects.se>
2012-04-29 19:35:57 +0200

    Adding google charts

--- ketchlip/views/onegram_view.py
+++ ketchlip/views/onegram_view.py
@@ -5,8 +5,28 @@ class OnegramView(BaseView):
         return \
 """
 <html>
-{% for e in word_count %}
-    <div>{{ e[0] }}: {{ e[1] }}</div>
-{% endfor %}
+    <head>
+        <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+        <script type="text/javascript">
+          google.load("visualization", "1", {packages:["corechart"]});
+          google.setOnLoadCallback(drawChart);
+          function drawChart() {
+            var data = google.visualization.arrayToDataTable([
+                ['Word', 'Count']
+                {% for e in word_count %},['{{ e[0] }}', {{ e[1] }}]{% endfor %}
+            ]);
+
+            var options = {
+              title: 'Word Count',
+              vAxis: {title: 'Count',  titleTextStyle: {color: 'blue'}}
+            };
+            var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+            chart.draw(data, options);
+          }
+        </script>
+    </head>
+    <body>
+        <div id="chart_div" style="width: 100%; height: 100%;"></div>
+    </body>
 </html>
 """
